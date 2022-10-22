a0a127c2ce07902be43e1f583252f5a64f8994d5
mohammedfajer <blueflameiraq@gmail.com>
2022-10-22 13:21:37 +0100

    #Comment updates

--- client/src/App.js
+++ client/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          BlueFlameIraq is comming soon baby ....
+          BlueFlameIraq
         </p>
         
       </header>
